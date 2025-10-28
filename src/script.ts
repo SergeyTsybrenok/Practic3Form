79fca4fbd4ad18e338f9ce2ff90628aeba8b6607
TheRose20 <cybrenok91@mail.ru>
2025-10-28 15:10:57 +0300

    Add some code (error, input, form)

--- src/script.ts
+++ src/script.ts
@@ -2,10 +2,44 @@ console.log("Hello world");
 
 const helloText = document.getElementById('hello-text') as HTMLElement;
 
-function GetRandomNumber () {
-    return Math.round(Math.random() * 100);
+const mainForm = document.getElementById('mainForm') as HTMLFormElement;
+
+const submitButton = document.getElementById('submitButton') as HTMLButtonElement;
+const errorText = document.getElementById('errorText') as HTMLElement;
+
+const inputs: string[] = ["userName", "distroName", "philosofy", "base packages", "license"];
+
+function AddError (newError: string) {
+    errorText.textContent += `\n ${newError}` 
 }
 
-helloText.addEventListener('click', function (e) {
-    helloText.textContent = GetRandomNumber().toString();
+// TODO mv to initialize all init inputs
+function CheckInputNotNull (inputs: string[]) {
+    inputs.forEach(inputString => {
+        const currentInput = document.getElementById(inputString) as HTMLInputElement;
+        const val = currentInput.value;
+
+        if (val === "") {
+            AddError(`${inputString} not be empty`)
+        }
+    });
+}
+
+function ClearErrorText () {
+    errorText.textContent = "";
+}
+
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    ClearErrorText();
+
+    CheckInputNotNull(inputs);
+
+    AddError(mainForm.userName.value);
+
 });
+
+function checkAll () {
+
+
+};
