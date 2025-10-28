a364668c6481ae6a6b0261a41602dc97c26ae112
TheRose20 <cybrenok91@mail.ru>
2025-10-28 21:05:38 +0300

    more code and catppuccin logo

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,93 @@
-console.log("Hello world");
+//#region ENUMS
+enum BasedOn {
+    RedHat,
+    Arch,
+    Gentoo,
+    Debian,
+    Ubuntu,
+    LFS,
+    Slackware
+}
 
-const helloText = document.getElementById('hello-text') as HTMLElement;
+enum InitSystem {
+    Systemd,
+    runit,
+    OpenRC,
+    Sinit,
+    s6
+}
 
+enum TypeOfUpdate {
+    LTS,
+    RollingRelese
+}
+
+enum PakageManager {
+    pacman,
+    nix,
+    dnf,
+    apt
+}
+//#endregion
+
+//#region COSNTANTS
 const mainForm = document.getElementById('mainForm') as HTMLFormElement;
 
 const submitButton = document.getElementById('submitButton') as HTMLButtonElement;
 const errorText = document.getElementById('errorText') as HTMLElement;
 
 const inputs: string[] = ["userName", "distroName", "philosofy", "base packages", "license"];
+//#endregion
+
+class UserData {
+    userName: string;
+    dateBirth: Date = new Date();
+
+    distroName: string;
+    basedOn: BasedOn;
+    auditory: string;
+    philosofy: string;
+    initSystem: InitSystem;
+    basePackages: string;
+    typeOfUpdate: TypeOfUpdate;
+    license: string;
+    packageManager: PakageManager;
+
+
+    constructor(
+        userName: string,
+        distroName: string,
+        basedOn: BasedOn,
+        auditory: string,
+        philosofy: string,
+        initSystem: InitSystem,
+        basePackages: string,
+        typeOfUpdate: TypeOfUpdate,
+        license: string,
+        packageManager: PakageManager)
+        {
+        this.userName = userName;
+        this.distroName = distroName;
+        this.basedOn = basedOn;
+        this.auditory = auditory;
+        this.initSystem = initSystem;
+        this.philosofy = philosofy;
+        this.basePackages = basePackages;
+        this.typeOfUpdate = typeOfUpdate;
+        this.license = license;
+        this.packageManager = packageManager;
+    };
+
+    TrySetDatebirth(newDate: Date) {
+        if (newDate.getTime() >= Date.now()) {
+            return false
+        }
+        else {
+            this.dateBirth = newDate;
+            return true
+        }
+    };
+};
 
 function AddError (newError: string) {
     errorText.textContent += `\n ${newError}` 
@@ -16,7 +96,7 @@ function AddError (newError: string) {
 // TODO mv to initialize all init inputs
 function CheckInputNotNull (inputs: string[]) {
     inputs.forEach(inputString => {
-        const currentInput = document.getElementById(inputString) as HTMLInputElement;
+        const currentInput = document.getElementById(inputString) as HTMLInputElement; //this one
         const val = currentInput.value;
 
         if (val === "") {
@@ -29,17 +109,15 @@ function ClearErrorText () {
     errorText.textContent = "";
 }
 
-submitButton.addEventListener('click', function (e) {
-    e.preventDefault();
+function TrySend () {
     ClearErrorText();
 
     CheckInputNotNull(inputs);
 
     AddError(mainForm.userName.value);
+}
 
+submitButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    TrySend();
 });
-
-function checkAll () {
-
-
-};
