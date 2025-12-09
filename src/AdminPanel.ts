195e58884faeffc94979e0e01db56a8cf5543d0b
TheRose20 <cybrenok91@mail.ru>
2025-12-09 18:38:20 +0300

    back button

--- src/AdminPanel.ts
+++ src/AdminPanel.ts
@@ -1,8 +1,19 @@
-const backButton = document.getElementById('backButton') as HTMLButtonElement;
+import * as BrowserTools from "./BrowserTools.js";
 
-// document.addEventListener('DOMContentLoaded', function (e) {
-// });
+type BrowserLocalInstance = InstanceType<typeof BrowserTools.BrowserLocal>;
+
+//#region CONSTANTS
+const browserLocal: BrowserLocalInstance = BrowserTools.BrowserLocal.getInstance();
+
+const backButton = document.getElementById('backButton') as HTMLButtonElement;
+const loadUserDataButton = document.getElementById('loadUserDataButton') as HTMLButtonElement;
+//#endregion
+
 
 backButton.addEventListener('click', function (e) {
     window.location.href = "index.html";
+});
+
+loadUserDataButton.addEventListener('click', function (e) {
+    console.log("Try load data");
 });
